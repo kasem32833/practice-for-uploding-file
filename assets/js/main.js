9ce38879a418c0be3ca03b79bb7226205c4d829a
kasem32833 <abulkasem32833@gmail.com>
2020-09-22 09:30:52 +0300

    This my first most completed PSD to HTML

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+    	$(".homepage-slides").owlCarousel({
+          
+            
+            items: 1,
+            nav: true,
+            dots: false,
+            autoplay: false,
+            loop: true,
+                   
+        });
+
+
+    });
+    
+    jQuery(window).load(function(){
+        
+    });
+}(jQuery));	
